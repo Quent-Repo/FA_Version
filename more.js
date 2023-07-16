1fce48eb1877383b50ab097e73c9a02ec0a3638c
grrkekisbest <97322615+grrkekisbest@users.noreply.github.com>
2023-07-15 23:25:47 -0400

    Update more.js
    works for both now did this in like 20min would have ben less is I had done it on the same pc but it is what it is. Also worked on first run so that's good to know

--- more.js
+++ more.js
@@ -1,7 +1,12 @@
 // document.getElementsByTagName("body")[0].dataset.staticPath // this can be used to tell which verson of the site you are using since both have different "ClassName" or buttons
+// There are two options '/themes/beta' or '/themes/classic'
+
+
 document.onkeydown = checkKey;
+//This is the beta theme
 function checkKey(See)
 	{
+	if(document.getElementsByTagName("body")[0].dataset.staticPath == '/themes/beta'){	
 		if(See.code === 'ArrowLeft')
 		{
 			if((document.getElementsByClassName("button standard mobile-fix")[0].text) == "Prev"){
@@ -16,3 +21,23 @@ function checkKey(See)
 		else{
 			null; }
 	}
+		else{
+			if(See.code === 'ArrowLeft')
+		{
+			
+			document.getElementsByClassName("next button-link")[0].click() //Older
+			
+		}
+		else if(See.code == 'ArrowRight'){
+			
+			document.getElementsByClassName("prev button-link")[0].click()//Older
+			
+			}
+		else{
+			null; }
+			
+		}
+	}
+
+document.getElementsByClassName("next button-link")[0].click()
+document.getElementsByClassName("prev button-link")[0].click()
